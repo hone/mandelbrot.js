5ccd4c6495564944d0b2a9b0b1d70ba43cf4c206
Terence Lee <hone02@gmail.com>
2017-11-05 22:59:20 -0600

    don't expose belongs publically

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-module.exports = {
-  belongs: function(c_re, c_im, maxIterations) {
-    var iteration = 0;
-    var x = 0.0;
-    var y = 0.0;
+function belongs(c_re, c_im, maxIterations) {
+  var iteration = 0;
+  var x = 0.0;
+  var y = 0.0;
 
-    for (iteration = 0; iteration < maxIterations; iteration++) {
-      // Calculate the real and imaginary components of the result
-      // separately
-      var x_new = (x * x - y * y) + c_re;
-      y = 2 * x * y + c_im;
-      x = x_new;
-      if(x * x + y * y > 2 * 2) {
-        return iteration/maxIterations * 100; // In the Mandelbrot set
-      }
+  for (iteration = 0; iteration < maxIterations; iteration++) {
+    // Calculate the real and imaginary components of the result
+    // separately
+    var x_new = (x * x - y * y) + c_re;
+    y = 2 * x * y + c_im;
+    x = x_new;
+    if(x * x + y * y > 2 * 2) {
+      return iteration/maxIterations * 100; // In the Mandelbrot set
     }
+  }
 
-    return 0.0; // Not in the set
-  },
+  return 0.0; // Not in the set
+}
+
+module.exports = {
   generate: function(width, height, maxIterations, magnification, panX, panY) {
     var t1 = Date.now();
     var image = new Uint8Array(width * height);
@@ -25,7 +26,7 @@ module.exports = {
       for(var y=0; y < height; y++) {
         var index = x * width + y;
         var belongsToSet =
-          this.belongs(x/magnification - panX,
+          belongs(x/magnification - panX,
               y/magnification - panY, maxIterations);
         image[index] = belongsToSet;
       }
